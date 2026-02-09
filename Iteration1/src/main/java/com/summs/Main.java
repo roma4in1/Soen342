9ef4ce5851734cbbb5619699e1714743d8c32123
roma4in1 <romainjeecorbel@gmail.com>
2026-02-09 16:05:21 -0500

    More

--- Iteration1/src/main/java/com/summs/Main.java
+++ Iteration1/src/main/java/com/summs/Main.java
@@ -2,7 +2,7 @@ package com.summs;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("SUMMS - Iteration 1");
-        System.out.println("Smart Urban Mobility Management System");
+        System.out.println("Iteration 1");
+        System.out.println("");
     }
 }
